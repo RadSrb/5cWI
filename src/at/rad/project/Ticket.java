755477db2cc604dc795ec2ecdbc68a60a4583ec5
RadSrb <97484817+RadSrb@users.noreply.github.com>
2024-10-21 11:14:09 +0200

    Update Ticket.java

--- src/at/rad/project/Ticket.java
+++ src/at/rad/project/Ticket.java
@@ -1,6 +1,39 @@
-package at.rad.project;
+package Ticketautomat;
+
+import java.util.Random;
 
 
 public class Ticket {
-    //Zaoo9
+    private int id;
+    private long createdTimeStamp;
+    private long payedTimeStamp;
+    private Random random = new Random();
+
+    public Ticket()
+    {
+        createdTimeStamp = System.currentTimeMillis();
+        id = random.nextInt();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public long getCreatedTimeStamp() {
+        return createdTimeStamp;
+    }
+
+    public void setCreatedTimeStamp(long createdTimeStamp) {
+        this.createdTimeStamp = createdTimeStamp;
+    }
+
+    public long getPayedTimeStamp() {
+        return payedTimeStamp;
+    }
+
+    public void setPayedTimeStamp(long payedTimeStamp) {
+        this.payedTimeStamp = payedTimeStamp;
+    }
+
+    
 }
