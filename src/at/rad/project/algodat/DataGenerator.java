a32dc77d23a131698be536facdcc9dfb14148d87
RadSrb <alex.radic@gmx.at>
2024-12-02 10:56:46 +0100

    data

--- src/at/rad/project/algodat/DataGenerator.java
+++ src/at/rad/project/algodat/DataGenerator.java
@@ -2,15 +2,43 @@ package at.rad.project.algodat;
 
 import java.util.Random;
 
-public class DataGenerator {
-    
+public class DataGenerator 
+{
     public static int[] generateDataArray(int size) {
-            Random rand = new Random();
-            int[] data = new int[size];
-            for (int i = 0; i < size; i++) {
-                data[i] = rand.nextInt();
-            }
-            return data;
+        Random random = new Random(); 
+        int[] data = new int[size];  
+        for (int i = 0; i < size; i++) 
+        {  
+            data[i] = random.nextInt();  
+        }
+        return data;  
     }
-    
+
+    public static int[] generateDataArray(int size, int min, int max) {
+        Random random = new Random(); 
+        int[] data = new int[size];  
+        for (int i = 0; i < size; i++) 
+        {  
+            data[i] = random.nextInt(min, max);  
+        }
+        return data;  
+    }
+
+    public static void printArray(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int a = 0; a < data.length-1; a++)
+        {
+            System.out.println(data[a]);
+        }
+    }
+
 }
