12bbcd1bb527bfcaf4b9c4135a308ce4fc939537
RadSrb <alex.radic@gmx.at>
2024-12-02 11:29:14 +0100

    dataGenerator

--- src/at/rad/project/algodat/DataGenerator.java
+++ src/at/rad/project/algodat/DataGenerator.java
@@ -19,7 +19,7 @@ public class DataGenerator
         int[] data = new int[size];  
         for (int i = 0; i < size; i++) 
         {  
-            data[i] = random.nextInt(min, max);  
+            data[i] = random.nextInt(max);
         }
         return data;  
     }
--- src/at/rad/project/algodat/Main.java
+++ src/at/rad/project/algodat/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int[] data1 =  DataGenerator.generateDataArray(3);
         int[] data2 =  DataGenerator.generateDataArray(8,0,100);
-        DataGenerator.printArray(data2);
+        DataGenerator.printArray(data1);
     }
 }
