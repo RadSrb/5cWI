c3194ee4f027e1b8cc0cfa264f0ff1ebef9ee65c
RadSrb <alex.radic@gmx.at>
2024-12-04 13:03:00 +0100

    commit

--- src/at/rad/project/algodat/DataGeneratorTest.java
+++ src/at/rad/project/algodat/DataGeneratorTest.java
@@ -1,20 +1,70 @@
 package at.rad.project.algodat;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class DataGeneratorTest {
+class DataGeneratorTest {
+
     @Test
-    void testGenerateArray() {
+    void generateArray_ShouldCreateArrayWithCorrectSize() {
+        // given
+        int size = 5;
 
+        // when
+        int[] result = DataGenerator.generateArray(size);
+
+        // then
+        assertEquals(size, result.length);
     }
 
     @Test
-    void testGenerateArrayInRange() {
+    void generateArray_ShouldCreateDifferentArraysOnMultipleCalls() {
+        // given
+        int size = 10;
 
+        // when
+        int[] firstArray = DataGenerator.generateArray(size);
+        int[] secondArray = DataGenerator.generateArray(size);
+
+        // then
+        boolean allElementsEqual = true;
+        for (int i = 0; i < size; i++) {
+            if (firstArray[i] != secondArray[i]) {
+                allElementsEqual = false;
+                break;
+            }
+        }
+        assertFalse(allElementsEqual, "Arrays should contain different values");
     }
 
     @Test
-    void testPrintArray() {
+    void generateArrayInRange_ShouldCreateArrayWithCorrectSize() {
+        // given
+        int size = 5;
+        int min = 1;
+        int max = 10;
 
+        // when
+        int[] result = DataGenerator.generateArrayInRange(size, min, max);
+
+        // then
+        assertEquals(size, result.length);
+    }
+
+    @Test
+    void generateArrayInRange_ShouldCreateArrayWithValuesInRange() {
+        // given
+        int size = 100; // larger size for better testing
+        int min = 5;
+        int max = 15;
+
+        // when
+        int[] result = DataGenerator.generateArrayInRange(size, min, max);
+
+        // then
+        for (int value : result) {
+            assertTrue(value >= min && value <= max,
+                    "Value " + value + " is outside the range [" + min + ", " + max + "]");
+        }
     }
 }
--- src/at/rad/project/algodat/Test.java
+++
@@ -1,7 +0,0 @@
-package at.rad.project.algodat;
-
-public class Test {
-public static void main(String[] args) {
-    System.out.println("i");
-}
-}
