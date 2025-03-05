cb2610254255cd9a7084c3a69ad1a01ca2093049
RadSrb <alex.radic@gmx.at>
2025-03-05 12:30:57 +0100

    javascript probleme

---
+++ src/at/rad/project/javascriptProblems/script.js
@@ -0,0 +1,7 @@
+function VariableTyp() {
+    console.log(1 + "1"); // "11" (String-Konkatenation)
+    console.log(1 - "1"); // 0 (String wird zu Zahl)
+    console.log(true + true); // 2 (true wird zu 1)
+    
+
+}
