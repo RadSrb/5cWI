4f2b4c329ac3d5e220a41e5e94cdca115ba47397
RadSrb <alex.radic@gmx.at>
2025-04-03 22:34:12 +0200

    DivisibleBy5 Finished

---
+++ src/at/rad/project/DivisibleBy5/DivisibleBy5.java
@@ -0,0 +1,16 @@
+package at.rad.project.DivisibleBy5;
+
+public class DivisibleBy5 {
+    public static void main(String[] args) {
+        for(int i = 1; i <=50;i++){
+            if(i % 5==0)
+            {
+                System.out.println("Divisible by 5");
+            }else{
+                System.out.println(i); 
+            }
+        
+        }
+    }
+
+}
