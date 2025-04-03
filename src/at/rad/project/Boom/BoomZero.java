a9b3835d0100365f607da0edde17569f5d32bbc8
RadSrb <alex.radic@gmx.at>
2025-04-03 22:45:32 +0200

    BoomZero Finished

---
+++ src/at/rad/project/Boom/BoomZero.java
@@ -0,0 +1,15 @@
+package at.rad.project.Boom;
+
+public class BoomZero {
+    
+public static void main(String[] args) {
+    for(int i = 1; i <= 100; i++){
+        if(i % 10 == 0){
+            System.out.println("Boom");
+        } else{
+            System.out.println(i);
+        }
+    }
+}
+
+}
