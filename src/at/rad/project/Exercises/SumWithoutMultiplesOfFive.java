a2040c6547907803bc50366150016f28391c3800
RadSrb <alex.radic@gmx.at>
2025-04-03 23:12:55 +0200

    SumWithoutMultiplesOf Five Finished

--- src/at/rad/project/Exercises/SumWithoutMultiplesOfFive.java
+++ src/at/rad/project/Exercises/SumWithoutMultiplesOfFive.java
@@ -3,12 +3,13 @@ package at.rad.project.Exercises;
 public class SumWithoutMultiplesOfFive {
 
     public static void main(String[] args) {
-        int sum =0;
+        int sum = 0;
         for (int i = 1; i <= 30; i++) {
             if (i % 5 != 0) {
                 sum += i;
             }
-        }System.out.println(sum);
+        }
+        System.out.println(sum);
     }
 
 }
