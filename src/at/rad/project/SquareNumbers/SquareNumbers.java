1906a3836ba09c250574eecbe60aa6f5d8ff73fe
RadSrb <alex.radic@gmx.at>
2025-04-03 23:03:14 +0200

    SquareNumbers Finished

---
+++ src/at/rad/project/SquareNumbers/SquareNumbers.java
@@ -0,0 +1,12 @@
+package at.rad.project.SquareNumbers;
+
+public class SquareNumbers {
+    
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10;i++){
+            int square = i * i;
+            System.out.println("The square of " + i + " is " + square);
+        }
+    }
+}
