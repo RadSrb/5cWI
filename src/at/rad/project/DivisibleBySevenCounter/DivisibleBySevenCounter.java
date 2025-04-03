ceb7c5957700cec3ae788dcef71adefacf748caa
RadSrb <alex.radic@gmx.at>
2025-04-03 22:43:22 +0200

    DivisbleBySevenCounter finished

---
+++ src/at/rad/project/DivisibleBySevenCounter/DivisibleBySevenCounter.java
@@ -0,0 +1,17 @@
+package at.rad.project.DivisibleBySevenCounter;
+
+public class DivisibleBySevenCounter {
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i <=50; i++) {
+
+            if (i % 7 == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+
+}
