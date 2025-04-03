d98360be18b1fda13e42715da56a1771f8e0645d
RadSrb <alex.radic@gmx.at>
2025-04-03 08:22:16 +0200

    javaScriptProblems

--- src/at/rad/project/javascriptProblems/script.js
+++ src/at/rad/project/javascriptProblems/script.js
@@ -1,7 +1,11 @@
-function VariableTyp() {
+
     console.log(1 + "1"); // "11" (String-Konkatenation)
     console.log(1 - "1"); // 0 (String wird zu Zahl)
-    console.log(true + true); // 2 (true wird zu 1)
-    
-
+    console.log(false + true); // 2 (true wird zu false)
+if (10 + true ){
+    console.log("inside");
 }
+else{
+    console.log("outside");
+}
+console.log(10+true);
